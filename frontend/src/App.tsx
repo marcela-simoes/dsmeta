e448661c7105de8ca7d852834424ca9f728b07f5
Marcela <p.marcelasimoes@gmail.com>
2022-07-16 20:33:59 -0300

    fix version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
       <NotificationButton />
       <NotificationButton />
     </>
